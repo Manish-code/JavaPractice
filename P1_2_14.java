301aa4eb50b195feb81f9964ae34199fd7bef27b
Manish Narayan <manish.amhs@gmail.com>
2019-09-16 21:32:56 +0530

    Create P1_2_14.java
    Solution to Question 1.2.14

---
+++ P1_2_14.java
@@ -0,0 +1,18 @@
+public class P1_2_14 {
+
+	public static void main(String[] args) {
+		
+		int a=16,b=8;
+		if( a%b ==0)
+		{
+			System.out.println("True");
+		}
+		
+		else
+		{
+			System.out.println("False");
+		}
+			
+	}
+
+}
